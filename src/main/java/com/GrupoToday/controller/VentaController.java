0565f68fa97544908d63df5bba327105e016534a
deanAnder <dean2anderhm@gmail.com>
2024-08-18 09:09:41 -0500

    has map VentaController

--- src/main/java/com/GrupoToday/controller/VentaController.java
+++ src/main/java/com/GrupoToday/controller/VentaController.java
@@ -2,8 +2,12 @@ package com.GrupoToday.controller;
 
 import com.GrupoToday.DTO.response.RequesVenta;
 import com.GrupoToday.service.VentaService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/venta")
@@ -14,8 +18,16 @@ public class VentaController {
     }
 
     @PostMapping("/boleta")
-    public ResponseEntity<?> registrarVenta(@RequestBody RequesVenta request){
-        ventaService.registrarVenta(request.getVentaDto(), request.getDetalles());
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Map<String, String>> registrarVenta(@RequestBody RequesVenta request){
+        Map< String, String> response = new HashMap<>();
+        try {
+
+            ventaService.registrarVenta(request.getVentaDto(), request.getDetalles());
+            response.put("message", "Venta Registrada");
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+        }catch (Exception e){
+            response.put("message", "Ocurrio un error");
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
