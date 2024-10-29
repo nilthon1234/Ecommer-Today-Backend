c3143b638b2447181492c4154e37eb1486eb3900
deanAnder <dean2anderhm@gmail.com>
2024-10-28 23:00:28 -0500

    avances de security todo

---
+++ src/main/java/com/GrupoToday/security/dto/MyReqRes.java
@@ -0,0 +1,20 @@
+package com.GrupoToday.security.dto;
+
+import com.GrupoToday.security.entity.Usuario;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MyReqRes {
+
+    private String token;
+    private String expirationTime;
+    private String name;
+    private String email;
+    private String role;
+    private String password;
+    private Usuario usuario;
+}
